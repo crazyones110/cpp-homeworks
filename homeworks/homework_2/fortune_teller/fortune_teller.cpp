47a988b373dff38bb4f9009a278850a84be5b177
crazyones110 <crazyones110@gmail.com>
2025-06-04 23:20:37 +0800

    using c++20 to finish the task

--- homeworks/homework_2/fortune_teller/fortune_teller.cpp
+++ homeworks/homework_2/fortune_teller/fortune_teller.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <array>
 #include <map>
+#include <format>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -15,7 +16,7 @@ int main()
       {"summer", "C++ expert"},
       {"autumn", "coding beast"},
       {"winter", "software design hero"}};
-  const std::vector<string> endings{
+  const std::array<string, 3> endings{
       "eats UB for breakfast",
       "finds errors quicker than the compiler",
       "is not afraid of C++ error messages"};
@@ -25,7 +26,6 @@ int main()
   string name{};
   cin >> name;
 
-  std::vector<string> adjectives{};
   cout << "Please enter the time of year when you were born:" << endl
        << "(pick from 'spring', 'summer', 'autumn', 'winter')" << endl;
   string season{};
@@ -36,15 +36,21 @@ int main()
   cout << "Please enter another adjective:" << endl;
   string adj2{};
   cin >> adj2;
-  adjectives.emplace_back(adj1);
-  adjectives.emplace_back(adj2);
+  const std::vector<string> adjectives{adj1, adj2};
 
   {
     cout << "Here is your description:" << endl;
-    std::size_t final_adj_index{name.length() % adjectives.size()};
+    const std::size_t final_adj_index{name.length() % adjectives.size()};
+    const std::size_t ending_index{name.length() % endings.size()};
 
-    cout << name << ", the " << adjectives.at(final_adj_index) << ' '
-    << seasons.at(season) << " that " << endings.at(name.length() % endings.size());
+    // cout << name << ", the " << adjectives.at(final_adj_index) << ' '
+    // << seasons.at(season) << " that " << endings.at(ending_index) << endl;
+    cout << std::format("{}, the {} {} that {}",
+      name,
+      adjectives.at(final_adj_index),
+      seasons.at(season),
+      endings.at(ending_index)
+    ) << endl;
   }
 
   return 0;
